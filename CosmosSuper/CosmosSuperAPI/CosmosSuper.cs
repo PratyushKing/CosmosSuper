51cef56ec884d58266b4c7e32ae98259315a1d09
Samma - PixelStudio <81291107+Samma2009@users.noreply.github.com>
2023-11-05 14:37:28 +0100

    creation of plug

--- CosmosSuper/CosmosSuperAPI/CosmosSuper.cs
+++ CosmosSuper/CosmosSuperAPI/CosmosSuper.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using CosmosSuper.API.Raws;
+using CosmosSuper.API.Raws;
+using CosmosSuper.API.Raws.Plugs;
 
 namespace CosmosSuper.API
 {
---
+++ CosmosSuper/CosmosSuperAPI/SoundBlasterPlug.cs
@@ -0,0 +1,15 @@
+﻿using Cosmos.HAL.Drivers.Audio;
+using IL2CPU.API.Attribs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosSuper.API.Raws.Plugs
+{
+    [Plug(Target = typeof(AC97))]
+    internal class SoundBlasterPlug
+    {
+    }
+}

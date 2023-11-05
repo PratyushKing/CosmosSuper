b868fab8bfe0df9ce879613d31efbc8c6245c8da
Samma - PixelStudio <81291107+Samma2009@users.noreply.github.com>
2023-11-05 14:32:43 +0100

    styles ideas

---
+++ CosmosSuper/CosmosSuperAPI/CosmosSuper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using CosmosSuper.API.Raws;
+
+namespace CosmosSuper.API
+{
+    public static class CosmosSuper
+    {
+
+        public static class SuperSound
+        {
+            
+            public static void Init()
+            {
+            
+                
+
+            }
+
+        }
+
+    }
+}
--- CosmosSuper/Kernel.cs
+++ CosmosSuper/Kernel.cs
@@ -11,6 +11,7 @@ namespace CosmosSuper
         protected override void BeforeRun()
         {
             Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            
         }
 
         protected override void Run()
